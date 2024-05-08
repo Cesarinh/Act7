87caf4d095612721cf1a8ad59c387bae80005abf
Cesarinh <160445469+Cesarinh@users.noreply.github.com>
2024-05-08 07:20:12 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,89 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'iCescMusic'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-  const MyHomePage({super.key, required this.title});  
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> imgList = [
+    "https://raw.githubusercontent.com/Cesarinh/img_flutterflow_IOS_6j/main/vice.jpg",
+    "https://raw.githubusercontent.com/Cesarinh/img_flutterflow_IOS_6j/main/x100.jpg",
+    "https://raw.githubusercontent.com/Cesarinh/img_flutterflow_IOS_6j/main/yhlq.jpg",
+    "https://raw.githubusercontent.com/Cesarinh/img_flutterflow_IOS_6j/main/yo.jpg",
+    "https://raw.githubusercontent.com/Cesarinh/img_flutterflow_IOS_6j/main/saturno.jpg",
+  ];
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      backgroundColor: Color(0xff9c07ff),
+      body: Column(
+        children: [
+          CarouselSlider(
+            items: imgList
+                .map((e) => Center(
+                      child: Image.network(e),
+                    ))
+                .toList(),
+            options: CarouselOptions(
+              initialPage: 0,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 2),
+              enlargeCenterPage: true,
+              enlargeFactor: 0.3,
+              onPageChanged: (value, _) {
+                setState(() {
+                  _currentPage = value;
+                });
+              },
+            ),
+          ),
+          buildCarouselIndicator(),
+        ],
       ),
+    );
+  }
+
+  Widget buildCarouselIndicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        for (int i = 0; i < imgList.length; i++)
+          Container(
+            margin: EdgeInsets.all(5),
+            height: i == _currentPage ? 7 : 5,
+            width: i == _currentPage ? 7 : 5,
+            decoration: BoxDecoration(
+              color: i == _currentPage ? Colors.black : Colors.grey,
+              shape: BoxShape.circle,
+            ),
+          ),
+      ],
     );
   }
 }
